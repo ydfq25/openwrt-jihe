b69c5733a4359ac64cb431c1f6c3f31078c8fb23
ydfq25 <155884087+ydfq25@users.noreply.github.com>
2024-01-14 06:50:50 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,7 +8,9 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#src-git small https://github.com/kenzok8/small
+sed -i '$a smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
